56d4db9dceeed52fe91bd16077d5ebd9a63793ad
Vincent Sjögren <vincentsjogren@gmail.com>
2022-09-12 18:38:11 +0200

    updated package.json, .gitignore, folder structure, type definition and added eslint

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,10 @@ import { Dispatch } from 'react';
  * @param throwOnError  optional boolean indicating if errors should be thrown when error is caught, default is false
  * @returns  null on failure or if item doesn't exist, decoded JSON if stored value is valid JSON, otherwise the value stored as is
  */
-const getStoredItem = (key: string, throwOnError: boolean = false): any => {
+const getStoredItem = <T = unknown>(
+	key: string,
+	throwOnError = false
+): T | null => {
 	try {
 		const value = localStorage.getItem(key);
 		if (value === null) return null;
@@ -28,15 +31,15 @@ const getStoredItem = (key: string, throwOnError: boolean = false): any => {
  * @param throwOnError  optional boolean indicating if errors should be thrown when error is caught, default is false
  * @returns  true on success, false on failure
  */
-const setStoredItem = (
+const setStoredItem = <T = unknown>(
 	key: string,
-	data: any,
-	throwOnError: boolean = false
+	data: T,
+	throwOnError = false
 ): boolean => {
 	try {
-		const value =
-			data instanceof Object || Array.isArray(data) ? encode(data) : data;
-		localStorage.setItem(key, value);
+		const value = typeof data === 'string' ? data : encode(data);
+		if (typeof value !== 'string') throw 'Invalid Value';
+		if (!value) localStorage.setItem(key, value);
 		return true;
 	} catch (e) {
 		if (throwOnError) throw e;
@@ -50,10 +53,7 @@ const setStoredItem = (
  * @param throwOnError  optional boolean indicating if errors should be thrown when error is caught, default is false
  * @returns  true on success, false on failure
  */
-const removeStoredItem = (
-	key: string,
-	throwOnError: boolean = false
-): boolean => {
+const removeStoredItem = (key: string, throwOnError = false): boolean => {
 	try {
 		localStorage.removeItem(key);
 		return true;
@@ -63,6 +63,8 @@ const removeStoredItem = (
 	}
 };
 
+export type InitialState<T> = T extends () => T ? () => T : T;
+
 /**
  * synchronize state with localStorage
  *
@@ -70,11 +72,11 @@ const removeStoredItem = (
  * @param initialState  optional parameter with initial state or function returning initial state. only stored in localStorage if nothing is already stored with the provided key or overwrite is set to true, default is null
  * @param overwrite  optional parameter indicates if initialState should overwrite already stored value, default is false
  */
-const useLocalStorage = (
+const useLocalStorage = <T = unknown>(
 	storageKey: string,
-	initialState: any | (() => any) = null,
-	overwrite: boolean = false
-): [any, Dispatch<any>] => {
+	initialState?: InitialState<T>,
+	overwrite = false
+): [T, Dispatch<React.SetStateAction<T>>] => {
 	const [didMount, setDidMount] = useState(false);
 
 	const [key, setKey] = useState(storageKey);
@@ -89,7 +91,8 @@ const useLocalStorage = (
 	});
 
 	useEffect(() => {
-		if (!didMount || storageKey === key) return;
+		if (!didMount) return setDidMount(true);
+		if (storageKey === key) return;
 		removeStoredItem(key);
 		setKey(storageKey);
 	}, [didMount, storageKey, key, setKey]);
@@ -97,10 +100,6 @@ const useLocalStorage = (
 	useEffect(() => {
 		setStoredItem(key, state);
 	}, [state, key]);
-
-	useEffect(() => {
-		setDidMount(true);
-	}, []);
 
 	return [state, setState];
 };
